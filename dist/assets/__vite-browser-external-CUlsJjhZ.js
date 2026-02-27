cd954e679784a0b581a2d5fc7e463baf8d415bf9
stagas <gstagas@gmail.com>
2026-02-27 16:22:50 +0200

    update build

--- dist/assets/__vite-browser-external-CUlsJjhZ.js
+++ dist/assets/__vite-browser-external-CUlsJjhZ.js
@@ -1,7 +1,7 @@
-import { t as __commonJSMin } from "./index-CAASWmlH.js";
+import { t as __commonJSMin } from "./index-B9DG0AoH.js";
 var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
 	module.exports = {};
 }));
 export default require___vite_browser_external();
 
-//# sourceMappingURL=__vite-browser-external-BYma9hdy.js.map
+//# sourceMappingURL=__vite-browser-external-CUlsJjhZ.js.map
