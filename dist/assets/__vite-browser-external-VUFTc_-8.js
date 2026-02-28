847244b60bf1147c884f2f8fe119c348d6555f2e
stagas <gstagas@gmail.com>
2026-02-28 13:59:30 +0200

    update build

---
+++ dist/assets/__vite-browser-external-VUFTc_-8.js
@@ -0,0 +1,7 @@
+import { t as __commonJSMin } from "./index-BBXN2JgM.js";
+var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
+	module.exports = {};
+}));
+export default require___vite_browser_external();
+
+//# sourceMappingURL=__vite-browser-external-VUFTc_-8.js.map
