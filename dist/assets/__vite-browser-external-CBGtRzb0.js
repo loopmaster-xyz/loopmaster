a268a695091dda00600143d494c0b36b0275568b
stagas <gstagas@gmail.com>
2026-03-03 11:38:57 +0200

    build: update

---
+++ dist/assets/__vite-browser-external-CBGtRzb0.js
@@ -0,0 +1,7 @@
+import { t as __commonJSMin } from "./index-ldMmOoyp.js";
+var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
+	module.exports = {};
+}));
+export default require___vite_browser_external();
+
+//# sourceMappingURL=__vite-browser-external-CBGtRzb0.js.map
