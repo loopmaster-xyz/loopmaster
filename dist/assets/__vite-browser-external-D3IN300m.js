00ee0b5897da6fc97eb7f3c40864333faaae5857
stagas <gstagas@gmail.com>
2026-03-03 04:51:54 +0200

    update build

---
+++ dist/assets/__vite-browser-external-D3IN300m.js
@@ -0,0 +1,7 @@
+import { t as __commonJSMin } from "./index-DEwkel2O.js";
+var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
+	module.exports = {};
+}));
+export default require___vite_browser_external();
+
+//# sourceMappingURL=__vite-browser-external-D3IN300m.js.map
