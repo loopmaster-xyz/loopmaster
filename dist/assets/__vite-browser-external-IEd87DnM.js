8dceded2d9657ffbce1a78e46debd9fcdd5b1ad9
stagas <gstagas@gmail.com>
2026-03-04 21:02:03 +0200

    build: update

--- dist/assets/__vite-browser-external-IEd87DnM.js
+++
@@ -1,7 +0,0 @@
-import { t as __commonJSMin } from "./index-CxYSZ3xf.js";
-var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
-	module.exports = {};
-}));
-export default require___vite_browser_external();
-
-//# sourceMappingURL=__vite-browser-external-IEd87DnM.js.map
