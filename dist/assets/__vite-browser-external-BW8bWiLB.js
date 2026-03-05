2856fff750e237b02f037dc5871adffb72a812ee
stagas <gstagas@gmail.com>
2026-03-05 14:52:22 +0200

    build: update

---
+++ dist/assets/__vite-browser-external-BW8bWiLB.js
@@ -0,0 +1,7 @@
+import { t as __commonJSMin } from "./index-MZK1yxuT.js";
+var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
+	module.exports = {};
+}));
+export default require___vite_browser_external();
+
+//# sourceMappingURL=__vite-browser-external-BW8bWiLB.js.map
