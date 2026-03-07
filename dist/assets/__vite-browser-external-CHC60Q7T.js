64db03a41a14935b70fa6493f93df991c870264d
stagas <gstagas@gmail.com>
2026-03-07 14:19:34 +0200

    build: update

---
+++ dist/assets/__vite-browser-external-CHC60Q7T.js
@@ -0,0 +1,7 @@
+import { t as __commonJSMin } from "./index-D6bDCBzQ.js";
+var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
+	module.exports = {};
+}));
+export default require___vite_browser_external();
+
+//# sourceMappingURL=__vite-browser-external-CHC60Q7T.js.map
