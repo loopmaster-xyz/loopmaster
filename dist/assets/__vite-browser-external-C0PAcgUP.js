0c6d50921f2494cad637aad216c0df06c1d11af0
stagas <gstagas@gmail.com>
2026-03-07 23:15:37 +0200

    build: update

---
+++ dist/assets/__vite-browser-external-C0PAcgUP.js
@@ -0,0 +1,7 @@
+import { t as __commonJSMin } from "./index-DtFIcmoy.js";
+var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
+	module.exports = {};
+}));
+export default require___vite_browser_external();
+
+//# sourceMappingURL=__vite-browser-external-C0PAcgUP.js.map
