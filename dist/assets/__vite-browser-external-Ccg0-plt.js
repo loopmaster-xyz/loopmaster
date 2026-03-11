4bab769819e657282d13c599f08fa81e6f46275f
stagas <gstagas@gmail.com>
2026-03-11 16:27:52 +0200

    build: update

---
+++ dist/assets/__vite-browser-external-Ccg0-plt.js
@@ -0,0 +1,7 @@
+import { t as __commonJSMin } from "./index-BWck5e5R.js";
+var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
+	module.exports = {};
+}));
+export default require___vite_browser_external();
+
+//# sourceMappingURL=__vite-browser-external-Ccg0-plt.js.map
--- dist/assets/__vite-browser-external-Dx7zjAgO.js
+++
@@ -1,7 +0,0 @@
-import { t as __commonJSMin } from "./index-CZd_4Wz4.js";
-var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
-	module.exports = {};
-}));
-export default require___vite_browser_external();
-
-//# sourceMappingURL=__vite-browser-external-Dx7zjAgO.js.map
