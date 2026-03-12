69f516127a8ee578c80e982479b3d2248cd03e37
stagas <gstagas@gmail.com>
2026-03-12 06:21:33 +0200

    build: update

---
+++ dist/assets/__vite-browser-external-CoTDZxap.js
@@ -0,0 +1,7 @@
+import { t as __commonJSMin } from "./index-CK-pF4r_.js";
+var require___vite_browser_external = /* @__PURE__ */ __commonJSMin(((exports, module) => {
+	module.exports = {};
+}));
+export default require___vite_browser_external();
+
+//# sourceMappingURL=__vite-browser-external-CoTDZxap.js.map
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,15 @@ import { cn } from '../lib/cn.ts'
 export const Main = ({ children, class: className }: { children?: preact.ComponentChildren; class?: string }) => {
   return (
     <div
-      class={cn('text-white/50 h-[calc(100dvh-50px)] overflow-y-scroll max-h-[calc(100dvh-50px)]', className)}
+      ref={el => {
+        if (el) {
+          requestAnimationFrame(() => {
+            el.focus()
+          })
+        }
+      }}
+      class={cn('text-white/50 h-[calc(100dvh-50px)] outline-none overflow-y-scroll max-h-[calc(100dvh-50px)]',
+        className)}
     >
       {children}
     </div>
